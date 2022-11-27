472943563ce9e427e2d4c30876fe0c621cda0ba9
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-27 00:07:36 -0300

    feat: add custom listener annotation

--- src/main/java/com/ohashi/strconsumer/listeners/StrConsumerListener.java
+++ src/main/java/com/ohashi/strconsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,27 @@
 package com.ohashi.strconsumer.listeners;
 
+import com.ohashi.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Received message: {}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Received message: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Received message: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Received message: {}", message);
     }
 }
