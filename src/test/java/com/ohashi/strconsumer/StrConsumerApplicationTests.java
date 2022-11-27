fc18398d21c10f9b688438584a9d6c87111e4f91
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-26 22:56:24 -0300

    first commit

---
+++ src/test/java/com/ohashi/strconsumer/StrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.strconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
